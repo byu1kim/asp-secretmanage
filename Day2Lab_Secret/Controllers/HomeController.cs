5cb220149fc369b1adcdba7a554cc8493b9eaf2d
Byul Kim <caxbelle@gmail.com>
2023-01-26 01:01:35 -0800

    Add project files.

---
+++ Day2Lab_Secret/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Day2Lab_Secret.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Day2Lab_Secret.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _configuration;
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        public IActionResult Index()
+        {
+            var adminUserName = _configuration["adminLogin:Username"];
+            var adminPassword = _configuration["AdminLogin:Password"];
+            var connectionString = _configuration["ConnectionStrings:DefaultConnection"];
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Day2Lab_Secret/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Day2Lab_Secret.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
