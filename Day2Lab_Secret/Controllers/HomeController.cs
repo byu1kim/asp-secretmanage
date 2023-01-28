6f1752a23de4f14b34c8f0a0b876cd6fd5037393
Byul Kim <caxbelle@gmail.com>
2023-01-27 20:19:54 -0800

    update

--- Day2Lab_Secret/Controllers/HomeController.cs
+++ Day2Lab_Secret/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using Day2Lab_Secret.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Text;
 
 namespace Day2Lab_Secret.Controllers
 {
@@ -16,10 +17,32 @@ namespace Day2Lab_Secret.Controllers
 
         public IActionResult Index()
         {
-            var adminUserName = _configuration["adminLogin:Username"];
-            var adminPassword = _configuration["AdminLogin:Password"];
-            var connectionString = _configuration["ConnectionStrings:DefaultConnection"];
-            return View();
+            string salt = "asdfja;lskjfaajf;2o3ujkewn02zxnaf";
+            string greetings = "Hello";
+            string message = $"Originally: {greetings}" +
+                                $", after MD5 conversion:" +
+                                $" {CreateMD5(greetings + salt)}";
+
+            return View("Index", message);
+        }
+
+        public static string CreateMD5(string input)
+        {
+            // Use input string to calculate MD5 hash
+            using (System.Security.Cryptography.MD5 md5 =
+                    System.Security.Cryptography.MD5.Create())
+            {
+                byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+                byte[] hashBytes = md5.ComputeHash(inputBytes);
+
+                // Convert the byte array to hexadecimal string
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < hashBytes.Length; i++)
+                {
+                    sb.Append(hashBytes[i].ToString("X2"));
+                }
+                return sb.ToString();
+            }
         }
 
         public IActionResult Privacy()
--- Day2Lab_Secret/Program.cs
+++ Day2Lab_Secret/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+//var connectionString = builder.Configuration.GetConnectionString("ApplicationDbContextConnection") ?? throw new InvalidOperationException("Connection string 'ApplicationDbContextConnection' not found.");
 
 // Add services to the container.
 
